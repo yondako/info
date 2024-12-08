14db587c03e91c8bf4322c9388a7857779ae0b11
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-12-08 20:28:52 +0900

    👍 時刻いらない

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -7,8 +7,5 @@ export function formatDate(date: Date): string {
   // 2ケタになるようにゼロ埋め
   const padZero = (num: number) => num.toString().padStart(2, "0");
 
-  const dateStr = `${date.getFullYear()}/${padZero(date.getMonth() + 1)}/${padZero(date.getDate())}`;
-  const timeStr = `${padZero(date.getHours())}:${padZero(date.getMinutes())}`;
-
-  return `${dateStr} ${timeStr}`;
+  return `${date.getFullYear()}/${padZero(date.getMonth() + 1)}/${padZero(date.getDate())}`;
 }
