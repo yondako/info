3359eaa332494d78df9822e0d237ef33f9f17869
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-12-14 23:07:13 +0900

    🚑 URLが終わってた

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "httpsL//info.yondako.com",
+  site: "https://info.yondako.com",
   integrations: [tailwind(), icon(), react()]
 });
 
